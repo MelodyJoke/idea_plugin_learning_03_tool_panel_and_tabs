1778cd5213bd26931f9cb576265edc707ef72e58
GitHub Action <action@github.com>
2024-02-06 03:48:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/melodyjoke/ideapluginlearning03toolpanelandtabs/MyBundle.kt
+++ src/main/kotlin/com/github/melodyjoke/ideapluginlearning03toolpanelandtabs/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.melodyjoke.ideapluginlearning03toolpanelandtabs
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/melodyjoke/ideapluginlearning03toolpanelandtabs/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/melodyjoke/ideapluginlearning03toolpanelandtabs/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.melodyjoke.ideapluginlearning03toolpanelandtabs.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
